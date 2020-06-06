7d9d56740939904860ff97821630f2d2a894bb7c
Ajay <ajaysh@policybazaar.com>
2020-06-06 22:33:37 +0530

    Updates

---
+++ precache-manifest.6efce739ab127a0059eaf00f6be983a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf7b4d20add4716aed1b58117ff36d3",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "578ae79f8ce37c65c2b3",
+    "url": "/corona-tracker/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "281214e53cdf504a64cb",
+    "url": "/corona-tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "578ae79f8ce37c65c2b3",
+    "url": "/corona-tracker/static/js/2.8160d914.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/corona-tracker/static/js/2.8160d914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281214e53cdf504a64cb",
+    "url": "/corona-tracker/static/js/main.b71f14d5.chunk.js"
+  },
+  {
+    "revision": "3978a030d47e50bd1e16",
+    "url": "/corona-tracker/static/js/runtime-main.9a79d275.js"
+  },
+  {
+    "revision": "28189628cf0c3c1c59c53367704dc601",
+    "url": "/corona-tracker/static/media/corona-virus.28189628.png"
+  }
+]);
